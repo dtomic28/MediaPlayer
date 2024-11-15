6f480e77e06c6b58a836c58ed418f86949d078f2
Danijel Tomić <danijeltomic2004@gmail.com>
2024-11-15 22:25:10 +0100

    Some progress done

---
+++ Backend/MultimediaFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer.Backend
+{
+    class MultimediaFile
+    {
+
+        private string filePath;
+        private string genre;
+        private string imgPath;
+        private string title;
+        private TimeSpan duration;
+        private string format;
+
+
+        public MultimediaFile(string filePath, string genre, string imgPath, string title, TimeSpan duration, string format)
+        {
+            this.filePath = filePath;
+            this.genre = genre;
+            this.imgPath = imgPath;
+            this.title = title;
+            this.duration = duration;
+            this.format = format;
+        }
+
+        public string FilePath => filePath;
+
+        public string Genre => genre;
+
+        public string ImgPath => imgPath;
+
+        public string Title => title;
+
+        public TimeSpan Duration => duration;
+
+        public string Format => format;
+    }
+}
--- Visual/MainWindow.xaml.cs
+++ Visual/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Diagnostics;
+﻿using MediaPlayer.Backend;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace MediaPlayer.Visual
 {
@@ -10,18 +13,43 @@ namespace MediaPlayer.Visual
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> playlist = new List<string> { "Song 1", "Song 2", "Song 3" };
+        //private List<string> playlist = new List<string> { "Song 1", "Song 2", "Song 3" };
+
+        private ObservableCollection<MultimediaFile> playlist = new ObservableCollection<MultimediaFile>
+        {
+            new MultimediaFile("C:\\Users\\danijel\\Videos\\Desktop\\Desktop 2020.05.26 - 19.03.47.01.mp4", "Pop", "C:\\Users\\danijel\\Pictures\\janez.png", "Song 1", new TimeSpan(0, 3, 45), "MP3"),
+            new MultimediaFile("C:/Music/song2.mp4", "Rock", "C:\\Users\\danijel\\Pictures\\janez.png", "Song 2", new TimeSpan(0, 4, 10), "MP4"),
+            new MultimediaFile("C:/Music/song3.wav", "Jazz", "C:\\Users\\danijel\\Pictures\\janez.png", "Song 3", new TimeSpan(0, 5, 0), "WAV")
+        };
+
+        private bool isPlaying = false;
+        private BitmapImage _playIcon = new BitmapImage(new Uri("pack://application:,,,/Resources/Icons/play_icon.ico"));
+        private BitmapImage _pauseIcon = new BitmapImage(new Uri("pack://application:,,,/Resources/Icons/pause_icon.ico"));
+
+
         public MainWindow()
         {
             InitializeComponent();
             Init();
+            PlayPauseIcon.Source = _playIcon;
+            MediaPlayer.Source = new Uri(playlist[0].FilePath);
+        }
+
+        private void PlayMedia(MultimediaFile file)
+        {
+            MediaTitle.Text = file.Title;
+            Debug.WriteLine(file.Title);
+        }
+
+        private void Reset()
+        {
+            MediaTitle.Clear();
         }
 
         public void Init()
         {
             CurrentTimeLabel.Content = "--:--";
             PlaylistView.ItemsSource = playlist;
-            MediaTitle.Text = "Kiša Metaka - Drai Millionen";
         }
 
         private void Import_Click(object sender, RoutedEventArgs e)
@@ -55,14 +83,9 @@ namespace MediaPlayer.Visual
 
         private void PlaylistView_DoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (PlaylistView.SelectedItem is ListViewItem selectedItem)
+            if ((PlaylistView.SelectedItem is MultimediaFile file))
             {
-                string? itemName = selectedItem.Content.ToString();
-                Debug.WriteLine(itemName);
-            }
-            else if(PlaylistView.SelectedItem is string itemString)
-            {
-                Debug.WriteLine(itemString);
+                PlayMedia(file);
             }
         }
 
@@ -71,6 +94,30 @@ namespace MediaPlayer.Visual
         }
 
 
+        private void StopBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            MediaTitle.Clear();
+            MediaPlayer.Stop();
+        }
 
+        private void PlayBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            isPlaying = !isPlaying;
+            PlayPauseIcon.Source = isPlaying ? _pauseIcon : _playIcon;
+
+            if (isPlaying)
+            {
+                MediaPlayer.Play();
+            }
+            else
+            {
+                MediaPlayer.Pause();
+            }
+        }
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
     }
+
 }
